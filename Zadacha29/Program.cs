02aa66d87ee34697e444d257b0686bfb680b2e2d
Polina <polinka29enp@gmail.com>
2022-08-18 21:44:22 +0300

    Zadacha29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+
+int[] array = new int[8]; 
+int number = array.Length;
+for (int i = 0; i < number; i++)
+{
+    array[i] = new Random().Next(0, 10);
+}
+ Console.WriteLine(string.Join(",", array));
